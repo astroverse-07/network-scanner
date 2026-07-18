ca292db4223ad35246123ccd0609339c39e7bea9
Abdullah Ghafoor <abdullahisgamer999@gmail.com>
2026-07-18 19:49:16 +0500

    Add error handling to classify ports as OPEN, CLOSED, or FILTERED

--- src/scanner.py
+++ src/scanner.py
@@ -3,14 +3,18 @@ import socket
 def scan_port(target_ip, port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(1)
-    result = s.connect_ex((target_ip, port))
-    s.close
-    if result == 0:
-        print(f"Port {port} is OPEN")
-    else:
-        print(f"Port {port} is CLOSED")
     
-#Test Run
-target = "scanme.nmap.org"
-scan_port(target, 80)
+    try:
+        result = s.connect_ex((target_ip, port))
+        if result == 0:
+            print(f"Port {port} is OPEN")
+        else:
+            print(f"Port {port} is CLOSED")
+    except socket.timeout:
+        print(f"Port {port} is FILTERED")
+    finally:
+        s.close()
 
+# Test run
+target = "scanme.nmap.org"
+scan_port(target, 443)
